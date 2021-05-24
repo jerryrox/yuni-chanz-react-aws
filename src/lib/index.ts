98c88a81e6f6aa4bd21364120476a6f0985cfcc5
Jerry Kim <jerryrox@live.co.kr>
2021-05-24 15:29:22 +1200

    Implemented AttributeExpression

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,7 @@
 export { default as AwsApi } from "./api/AwsApi";
 export type { default as IAwsApiConfig } from "./api/IAwsApiConfig";
 
+export { default as AttributeExpression } from "./data/AttributeExpression";
 export { default as AwsModelConverter } from "./data/AwsModelConverter";
+
+export { default as DdbUtils } from "./utils/DdbUtils";

1b249e9a956088e401561dc63f39c81c3de6e21c
Jerry Kim <jerryrox@live.co.kr>
2021-05-24 12:30:09 +1200

    Implemented more encode/decode functions for AwsModelConverter.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 AWS integration plugin for yuni-chanz-react.
 
 ## Versions
+### 0.0.2
+#### New features
+- Added more encode/decode functions in AwsModelConverter.
+
 ### 0.0.1
 #### New features
 - Implemented abstract API classes for AWS integration.
--- src/lib/data/AwsModelConverter.ts
+++ src/lib/data/AwsModelConverter.ts
@@ -1,11 +1,138 @@
 import { ModelConverter } from "yuni-chanz-react";
+import { AttributeValue as DdbAttributeValue } from "@aws-sdk/client-dynamodb";
+import { AttributeValue as DdbsAttributeValue } from "@aws-sdk/client-dynamodb-streams";
 
 export default abstract class AwsModelConverter<T = any> extends ModelConverter<T> {
     
     /**
+     * Encodes the specified number value to a DDB attribute value.
+     */
+    encodeNumber(value: number): DdbAttributeValue | DdbsAttributeValue {
+        return {
+            N: value.toString(),
+        };
+    }
+
+    /**
+     * Encodes the specified string value to a DDB attribute value.
+     */
+    encodeString(value: string): DdbAttributeValue | DdbsAttributeValue {
+        return {
+            S: value,
+        };
+    }
+
+    /**
+     * Encodes the specified bool value to a DDB attribute value.
+     */
+    encodeBool(value: boolean): DdbAttributeValue | DdbsAttributeValue {
+        return {
+            BOOL: value,
+        };
+    }
+
+    /**
+     * Encodes the specified string array value to a DDB attribute value.
+     */
+    encodeStringArray(value: string[]): DdbAttributeValue | DdbsAttributeValue {
+        return {
+            SS: value,
+        };
+    }
+
+    /**
+     * Encodes the specified number array value to a DDB attribute value.
+     */
+    encodeNumberArray(value: number[]): DdbAttributeValue | DdbsAttributeValue {
+        return {
+            NS: value.map((v) => v.toString()),
+        };
+    }
+
+    /**
      * Encodes the specified date instance to a storable type.
      */
-    encodeDate(date: Date): string {
-        return date.toISOString();
+    encodeDate(value: Date): DdbAttributeValue | DdbsAttributeValue {
+        return {
+            S: value.toISOString(),
+        };
+    }
+
+    /**
+     * Decodes the specified raw value to an int.
+     */
+    decodeInt(value: (DdbAttributeValue | DdbsAttributeValue)): number | null {
+        return super.decodeInt(value.N);
+    }
+
+    /**
+     * Decodes the specified raw value to a float.
+     */
+    decodeFloat(value: (DdbAttributeValue | DdbsAttributeValue)): number | null {
+        return super.decodeFloat(value.N);
+    }
+
+    /**
+     * Decodes the specified raw value to a string.
+     */
+    decodeString(value: (DdbAttributeValue | DdbsAttributeValue)): string | null {
+        return super.decodeString(value.S);
+    }
+
+    /**
+     * Decodes the specified raw value to a bool.
+     */
+    decodeBool(value: (DdbAttributeValue | DdbsAttributeValue)): boolean | null {
+        return super.decodeBool(value.BOOL);
+    }
+
+    /**
+     * Decodes the specified raw value to a string array.
+     */
+    decodeStringArray(value: (DdbAttributeValue | DdbsAttributeValue)): string[] | null {
+        return value.SS ?? null;
+    }
+
+    /**
+     * Decodes the specified raw value to an integer array.
+     */
+    decodeIntArray(value: (DdbAttributeValue | DdbsAttributeValue), radix = 10): number[] | null {
+        if(value.NS === undefined) {
+            return null;
+        }
+        const intArray: number[] = [];
+        for(const v of value.NS) {
+            const val = super.decodeInt(v, radix);
+            if(val === null) {
+                return null;
+            }
+            intArray.push(val);
+        }
+        return intArray;
+    }
+
+    /**
+     * Decodes the specified raw value to a float array.
+     */
+    decodeFloatArray(value: (DdbAttributeValue | DdbsAttributeValue)): number[] | null {
+        if(value.NS === undefined) {
+            return null;
+        }
+        const intArray: number[] = [];
+        for(const v of value.NS) {
+            const val = super.decodeFloat(v);
+            if(val === null) {
+                return null;
+            }
+            intArray.push(val);
+        }
+        return intArray;
+    }
+
+    /**
+     * Decodes the specified raw value to a Date instance.
+     */
+    decodeDate(value: (DdbAttributeValue | DdbsAttributeValue)): Date | null {
+        return super.decodeDate(value.S);
     }
 }
